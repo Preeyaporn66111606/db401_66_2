6e81c93df5cd5a820043ac6d843499a4f5621bf0
Preeyaporn66111606 <66111606@dpu.ac.th>
2024-03-06 12:06:05 +0700

    pubspec.yaml

---
+++ tictactoe/lib/pages/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _Loginstate();
+}
+
+class _Loginstate extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ tictactoe/lib/pages/register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _Registerstate();
+}
+
+class _Registerstate extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
